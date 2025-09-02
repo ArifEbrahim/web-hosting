10832bb56722468c9adf5f802c7b4ba3895b4bae
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2025-09-02 08:06:37 +0100

    react router working

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import Home from './pages'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>hello world</StrictMode>
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
