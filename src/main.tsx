9b09451a1cb5654694ba2188dd501d04a6069676
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2025-09-26 07:13:46 +0100

    added css to home page

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter } from 'react-router'
 import { RouterProvider } from 'react-router/dom'
 import Home from './pages'
+import './main.css'
 
 const router = createBrowserRouter([
   {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
-export default function Home(){
-  return <div>hello world</div>
+import styles from './home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <section>
+        <h1>Get the freedom you deserve.</h1>
+      </section>
+      <section>
+        <h1>Chose your plan</h1>
+      </section>
+    </>
+  )
 }
